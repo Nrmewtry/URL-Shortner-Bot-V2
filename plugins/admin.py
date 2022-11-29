34185e56119fed537ad2a4ebb95055a2e134171b
Nrmewtry <112067383+Nrmewtry@users.noreply.github.com>
2022-11-29 17:00:28 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -20,7 +20,7 @@ class Database:
     
     def __init__(
 	self,
-	url=os.mongodb+srv://tryit:Try!1233@cluster0.afuhrkj.mongodb.net/?retryWrites=true&w=majority,
+	url=mongodb+srv://tryit:Try!1233@cluster0.afuhrkj.mongodb.net/?retryWrites=true&w=majority,
 	database_name="FnURLShortBot"
     ):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(url)
